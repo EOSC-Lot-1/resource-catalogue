eec7be04db882b9595bc5899092e7a953fadbb05
nleond <nikleonidakis@gmail.com>
2024-09-08 19:49:28 +0300

    Add publish trigger missing types

---
+++ resource-catalogue-model/src/main/java/gr/uoa/di/madgik/resourcecatalogue/dto/EnumVariableType.java
@@ -0,0 +1,13 @@
+package gr.uoa.di.madgik.resourcecatalogue.dto;
+
+public enum EnumVariableType {
+    STRING,
+    BOOLEAN,
+    INTEGER,
+    LONG,
+    DOUBLE,
+    DATE,
+    DATE_TIME,
+    JSON
+    ;
+}
